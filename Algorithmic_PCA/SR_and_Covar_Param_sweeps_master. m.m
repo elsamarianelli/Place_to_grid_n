3a7267cf4856b7b6eebec7c245d59b048e17e689
elsamarianelli <zcbtetm@ucl.ac.uk>
2025-05-30 17:58:19 +0100

    p

--- Algorithmic_PCA/SR_and_Covar_Param_sweeps_master. m.m
+++ Algorithmic_PCA/SR_and_Covar_Param_sweeps_master. m.m
@@ -50,7 +50,7 @@ In.NumberOfPC = In.n_cells; % number of princ comps - should be the same as the 
 In.bound_ctrl = 2;
             
 % Folder naming tags - vary according to settings
-base_dir = 'grids_data'; 
+base_dir = 'grids_data_new'; 
 method_tag = 'SR'; if ~use_SR; method_tag = 'covar'; end
 traj_tag = use_traj;  
 density_tag = 'densityVaried' ; if ~ pc_density; density_tag = 'densityConstant'; end
